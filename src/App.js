10c2b866c18c59ce7df805f28b583b1efbb0ef83
escowin <edwin@escowinart.com>
2022-04-29 09:31:52 -0500

    20.3.4 | gallery rendering & document.title hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
+import Gallery from './components/Gallery';
 
 function App() {
+  const [categories] = useState([
+    { name: "commercial", description: "photos of grocery stores, food trucks, and other commercial projects",},
+    { name: "portraits", description: "portraits of people in my life" },
+    { name: "food", description: "delicious delicacies" },
+    { name: "landscape", description: "fields, farmhouses, waterfalls, and the beauty of nature" },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
     <div>
-      <Nav />
+      <Nav categories={categories} setCurrentCategory={setCurrentCategory} currentCategory={currentCategory}/>
       <main>
-          <About />        
+        <div>
+          <Gallery />
+          <About /> 
+        </div>
       </main>
     </div>
   );
---
+++ src/components/Gallery/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import photo from "../../assets/small/commercial/0.jpg"
+
+function Gallery(props) {
+    const currentCategory = {
+        name: "commercial",
+        description: "phots of grocery stores, food trunks, and other commercial projects",
+    };
+    return (
+        <section>
+            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+            <p>{currentCategory.name}</p>
+            <div className='flex-row'>
+                <img src={photo} alt="commercial example" className='img-thumbnail mx-1'/>
+            </div>
+        </section>
+    )
+}
+
+export default Gallery;
