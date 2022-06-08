c3e37ad51ea7f7f5d022e0f87d338ab3d5058feb
escowin <edwin@escowinart.com>
2022-06-08 12:04:08 -0500

    onBlur

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -43,16 +43,21 @@ function ContactForm() {
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">name:</label>
-          <input type="text" name="name" defaultValue={name} onChange={handleChange} />
+          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
         </div>
         <div>
           <label htmlFor="email">email address:</label>
-          <input type="email" name="email" defaultValue={email} onChange={handleChange} />
+          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
         </div>
         <div>
           <label htmlFor="message">message:</label>
-          <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
+          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
         </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
         <button type="submit">submit</button>
       </form>
     </section>
