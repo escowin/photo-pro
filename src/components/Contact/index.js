50c09c1e3f2e69dcf864335b882e7408c7ef12d6
escowin <edwin@escowinart.com>
2022-06-08 09:41:54 -0500

    20.4.3 | jsx contact <form />

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,47 +1,27 @@
-import React, { useState } from "react";
-import { validateEmail } from '../../utils/helpers';
+import React from "react";
 
 function ContactForm() {
-  const [formState, setFormState] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-  const { name, email, message } = formState;
-
-  function handleChange(e) {
-    setFormState({ ...formState, name: e.target.value });
-  }
-  console.log(formState);
-
-  
-
   // JSX
   return (
     <section>
-      <h1>Contact Me</h1>
+      <h1>contact me</h1>
       <form id="contact-form">
         <div>
-          <label htmlFor="name">name</label>
-          <input
-            type="text"
-            name="name"
-            defaultValue={name}
-            onChange={handleChange}
-          />
+          <label htmlFor="name">name:</label>
+          <input type="text" name="name"/>
         </div>
         <div>
-          <label htmlFor="email">email address</label>
-          <input type="email" name="email" defaultValue={email} />
+          <label htmlFor="email">email address:</label>
+          <input type="email" name="email" />
         </div>
         <div>
-          <label htmlFor="message">message</label>
-          <input type="message" rows="5" defaultValue={message} />
+          <label htmlFor="message">message:</label>
+          <textarea name="message" rows="5" />
         </div>
         <button type="submit">submit</button>
       </form>
     </section>
-  );
+  )
 }
 
 export default ContactForm;
